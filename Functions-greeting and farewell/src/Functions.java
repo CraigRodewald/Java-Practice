9d25d75cb2f8fe221effcac959a1439e18c06830
CraigRodewald <craig.rodewald@gmail.com>
2016-03-24 14:35:24 -0400

    added simple function program- returns hello/goodbye

---
+++ Functions-greeting and farewell/src/Functions.java
@@ -0,0 +1,17 @@
+
+public class Functions {
+	public static void main(String[] args) {
+		sayHello();
+		sayGoodbye();
+	}
+
+	private static void sayGoodbye() {
+		// TODO Auto-generated method stub
+		System.out.println("Good Bye!");
+	}
+
+	private static void sayHello() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!");
+	}
+}
